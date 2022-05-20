2cfabb069c836ff0d747bab68121d4f210ccec3f
Wagner shy <Wagnerpedro355@gmail.com>
2022-05-20 14:55:36 +0100

    módulo

---
+++ js/changeTheme.js
@@ -0,0 +1,37 @@
+export default changeTheme;
+
+//dark mode function
+function changeTheme() {
+
+    //muda a cor e o fundo do body
+    document.body.style.backgroundColor = "#1b1b1b";
+    document.getElementsByTagName("body")[0].style.color = "#a8a8a8";
+
+    //muda a cor dos botões
+    (() => {
+        document.getElementsByTagName("button")[0].style.color = "#a8a8a8";
+        document.getElementsByTagName("button")[1].style.color = "#a8a8a8";
+        document.getElementsByTagName("button")[2].style.color = "#a8a8a8";
+    })();
+    
+    //muda a fundo dos botões
+    (() => {
+        document.getElementsByTagName("button")[0].style.backgroundColor = "#1b1b1b";
+        document.getElementsByTagName("button")[1].style.backgroundColor = "#1b1b1b";
+        document.getElementsByTagName("button")[2].style.backgroundColor = "#1b1b1b";
+    })();
+
+    //muda cor do h1
+    (() => {
+        document.querySelector("h1").style.color = "#a8a8a8";
+    })();
+
+    //muda a cor do textares
+    (() => {
+        document.querySelector("textarea").style.color = "#a8a8a8";
+    })();
+
+    document.querySelector("button.btnThemes").innerHTML = "whiteMode";
+    
+    document.querySelector("button.btnThemes").addEventListener("click", ()=> window.location.reload())
+}
--- js/wodo.js
+++ js/wodo.js
@@ -1,39 +1,5 @@
-//dark mode function
-function changeTheme() {
-
-    //muda a cor e o fundo do body
-    document.body.style.backgroundColor = "#1b1b1b";
-    document.getElementsByTagName("body")[0].style.color = "#a8a8a8";
-
-    //muda a cor dos botões
-    (() => {
-        document.getElementsByTagName("button")[0].style.color = "#a8a8a8";
-        document.getElementsByTagName("button")[1].style.color = "#a8a8a8";
-        document.getElementsByTagName("button")[2].style.color = "#a8a8a8";
-    })();
-    
-    //muda a fundo dos botões
-    (() => {
-        document.getElementsByTagName("button")[0].style.backgroundColor = "#1b1b1b";
-        document.getElementsByTagName("button")[1].style.backgroundColor = "#1b1b1b";
-        document.getElementsByTagName("button")[2].style.backgroundColor = "#1b1b1b";
-    })();
-
-    //muda cor do h1
-    (() => {
-        document.querySelector("h1").style.color = "#a8a8a8";
-    })();
-
-    //muda a cor do textares
-    (() => {
-        document.querySelector("textarea").style.color = "#a8a8a8";
-    })();
-
-    document.querySelector("button.btnThemes").innerHTML = "whiteMode";
-    
-    document.querySelector("button.btnThemes").addEventListener("click", ()=> window.location.reload())
-}
-
+//importando a função de mudança de tema
+import changeTheme from '../js/changeTheme.js';
                         //MAIN CODE
 
 // add a item on ul
